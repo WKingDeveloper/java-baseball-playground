6981bcb74461571f85ec0314d6111350494059d3
WKingDev <wkingdeveloper@gmail.com>
2022-10-03 21:19:01 +0900

    refactor : ball 도메인 생성 후 ball 관련 로직을 관리하게함
    - main 동작 함수인 Baseball에서 진행되던 기능들을 ball로 옮김

--- src/main/java/study/Baseball.java
+++ src/main/java/study/Baseball.java
@@ -1,28 +1,27 @@
 package study;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
 
 public class Baseball {
 
     public static void main(String[] args) {
 
-        Baseball baseball = new Baseball();
+        Ball ball = new Ball();
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
+
         String[] answers = null;
-        boolean isReset = true;
         boolean isRunGame = true;
+        boolean isReset = true;
         boolean isReGame = true;
+
         while (isRunGame){
 
             if (isReset) {
                 boolean isValidAnswers = false;
                 while (!isValidAnswers){
-                    answers = baseball.createAnswer();
-                    isValidAnswers = baseball.validNumbers(answers);
+                    answers = ball.createAnswer();
+                    isValidAnswers = ball.validNumbers(answers);
                 }
                 String answersToString = Arrays.toString(answers);
                 System.out.println("정답 : " + answersToString);
@@ -33,14 +32,14 @@ public class Baseball {
             String reGameResult;
             boolean isValidInputNumbers = false;
             String inputNumber = inputView.inputMessage();
-            isValidInputNumbers = baseball.validInputNumbers(inputNumber);
+            isValidInputNumbers = ball.validInputNumbers(inputNumber);
 
             if (!isValidInputNumbers) {
                 inputView.notValidInputNumbersMessage();
                 continue;
             }
 
-            Integer[] predictionResult = baseball.validAnswer(answers, inputNumber);
+            Integer[] predictionResult = ball.validAnswer(answers, inputNumber);
             isAnswer = resultView.predictionResultMessage(predictionResult);
 
             if (isAnswer) {
@@ -56,72 +55,7 @@ public class Baseball {
 
     }
 
-    public String[] createAnswer() {
-        String[] answers = new String[3];
-        List<String> candidateAnswers = createCandidateAnswers();
-        answers = selectAnswers(answers, candidateAnswers);
-        return answers;
-    }
 
-    public boolean validNumbers(String[] result) {
-        boolean isValid = true;
-        if(result.length != 3){
-            isValid = false;
-            return isValid;
-        }
-        if(result[0]==result[1] ||result[0]==result[2] || result[2]==result[1]){
-            isValid = false;
-        }
-        return isValid;
-    }
 
-    public boolean validInputNumbers(String result) {
-        boolean isValid = true;
-        if(result.length() != 3){
-            isValid = false;
-            return isValid;
-        }
-        if(result.charAt(0) == result.charAt(1) ||
-                result.charAt(0) == result.charAt(2) ||
-                result.charAt(1) == result.charAt(2)) {
-            isValid = false;
-        }
-        return isValid;
-    }
-
-    private List<String> createCandidateAnswers() {
-        List<String> candidateAnswers = new ArrayList();
-
-        for (int i = 1; i < 10; i++) {
-            candidateAnswers.add(String.valueOf(i));
-        }
-        return candidateAnswers;
-    }
-
-    private String[] selectAnswers(String[] answers, List<String> candidateAnswers) {
-        Random random = new Random();
-        for (int i = 0; i < 3; i++) {
-            int num = random.nextInt(9-i);
-            answers[i] = candidateAnswers.get(num);
-            candidateAnswers.remove(num);
-        }
-        return answers;
-    }
-
-    public Integer[] validAnswer(String[] answers, String question) {
-        String answer = answers[0] + answers[1] + answers[2];
-        Integer[] result = {0,0};
-        for (int i = 0; i < 3; i++) {
-            String q = String.valueOf(question.charAt(i));
-            if (answers[i].equals(q)) {
-                result[1] += 1;
-                continue;
-            }
-            if (answer.contains(q)) {
-                result[0] += 1;
-            }
-        }
-        return result;
-    }
 
 }
--- src/test/java/study/BaseballTest.java
+++ src/test/java/study/BaseballTest.java
@@ -9,16 +9,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BaseballTest {
 
-    Baseball baseball = new Baseball();
-
+    Ball ball = new Ball();
 
     @Test
     @DisplayName("각기 다른 3자리 숫자가 랜덤으로 나오게 하기")
     void createAnswerTest() {
-        String[] result = baseball.createAnswer();
-        boolean isAnswerValid = baseball.validNumbers(result);
-        String resultToString = Arrays.toString(result);
-        System.out.println(resultToString);
+        String[] result = ball.createAnswer();
+        boolean isAnswerValid = ball.validNumbers(result);
         assertThat(result.length).isEqualTo(3);
         assertThat(isAnswerValid).isEqualTo(true);
     }
@@ -28,22 +25,22 @@ class BaseballTest {
     void validNumberIsFalse() {
         String[] result1 = {"1", "2", "3", "4"};
         String[] result2 = {"2", "5", "5"};
-        boolean isAnswerValid1 = baseball.validNumbers(result1);
-        boolean isAnswerValid2 = baseball.validNumbers(result2);
+        boolean isAnswerValid1 = ball.validNumbers(result1);
+        boolean isAnswerValid2 = ball.validNumbers(result2);
         assertThat(isAnswerValid1).isEqualTo(false);
         assertThat(isAnswerValid2).isEqualTo(false);
     }
 
     @Test
-    @DisplayName("3자리 숫자 valid 실패한 경우")
+    @DisplayName("3자리 숫자 입력 후 결과 확인")
     void validAnswerTest() {
         String[] answer = {"2", "5", "9"};
         String q1 = "345";
         String q2 = "239";
         String q3 = "146";
-        Integer[] resut1 = baseball.validAnswer(answer,q1);
-        Integer[] resut2 = baseball.validAnswer(answer,q2);
-        Integer[] resut3 = baseball.validAnswer(answer,q3);
+        Integer[] resut1 = ball.validAnswer(answer,q1);
+        Integer[] resut2 = ball.validAnswer(answer,q2);
+        Integer[] resut3 = ball.validAnswer(answer,q3);
         assertThat(resut1).isEqualTo(new Integer[]{1,0});
         assertThat(resut2).isEqualTo(new Integer[]{0,2});
         assertThat(resut3).isEqualTo(new Integer[]{0,0});
